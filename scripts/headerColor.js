41e4ef75e14c36e40b2574d7d7ae1695e814e6dc
ARKXII <jebvillalobos@gmail.com>
2026-01-29 15:20:09 +1100

    added back headerColor.js

--- scripts/headerColor.js
+++ scripts/headerColor.js
@@ -1,9 +1,29 @@
+const currentURL = window.location.href;
+
 // if test environment, change header color
-function changeHeaderColor() {
+function changeHeaderColor(string) {
   const header = document.getElementById("global-navigation");
-  if (header) {
-    header.style.cssText += "background-color: #b99a0e !important;";
-  } else return false;
+  const actionStepPattern =
+    /^https:\/\/ap-southeast-2\.actionstep(staging)?\.com\//;
+  const match = string.match(actionStepPattern);
+
+  let environment = match[1] === "staging" ? "staging" : "production";
+  if (!match) return false;
+
+  if (environment === "staging") {
+    if (header) {
+      header.style.cssText += "background-color: #b99a0e !important;";
+    } else return false;
+  } else if (environment === "production") {
+    if (header) {
+      header.style.cssText += "background-color: #004b87 !important;";
+    } else return false;
+  }
+
+  const globalMessage = document.getElementById("global-message");
+  if (globalMessage) {
+    globalMessage.style.cssText += "position: sticky; top:0; z-index: 9999;";
+  }
 }
 
-changeHeaderColor();
+changeHeaderColor(currentURL);
